d2384912610e20d04012c0caad0797e270105839
Ryan Valera <valeraryan@gmail.com>
2022-10-09 16:00:20 -0700

    Started adding css properties and theme/styling

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+let score = [0,0];
+let activePlayer = 1;
+
+const player = function (name, idNum) {
+    // DOM elements
+
+    // the sum of points pushed to the
+    let playerScore = 0;
+    let currentScore = 0;
+    let dice = [];
+
+    const getName = function () {
+        // code
+    };
+
+    // return current score
+    const updateCurrent = function(diceTotal) {
+        // code
+    }
+
+    const getScore = function() {
+        // code
+    };
+
+    // DISABLE THE BUTTON FOR PLAYER 1 PLAYER 2 ON HOLD
+    const holdScore = function() {}
+
+    const roll = function() {
+        // code
+    };
+
+    // current points before added to the playerScore
+    const currentPoints = {
+        // code
+    };
+
+    return {getName, roll};
+}
+
