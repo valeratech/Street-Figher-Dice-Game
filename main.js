b188903bfaf7554657567a76015d5aeaae49e0b1
Ryan Valera <valeraryan@gmail.com>
2022-10-11 20:46:52 -0700

    Added function to roll dice and have the jpg images for each side(dice) display in the main container

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let activePlayer = 1;
 
 const player = function (name, idNum) {
     // DOM elements
+    const diceOne = document.querySelector('.dice--1')
+    const diceTwo = document.querySelector('.dice--2')
+    const curContainer = document.querySelector(`#current--${idNum}`)
 
     // the sum of points pushed to the
     let playerScore = 0;
@@ -16,9 +19,7 @@ const player = function (name, idNum) {
     };
 
     // return current score
-    const updateCurrent = function(diceTotal) {
-        // code
-    }
+
 
     const getScore = function() {
         // code
@@ -28,7 +29,12 @@ const player = function (name, idNum) {
     const holdScore = function() {}
 
     const roll = function() {
-        // code
+        for (let x = 0; x < 2; x++) {
+            dice[x] = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+        }
+        const diceTotal = dice.reduce((a,b) => a + b, 0);
+        diceOne.src = `dice-${dice[0]}.png`;
+        diceTwo.src = `dice-${dice[1]}.png`;
     };
 
     // current points before added to the playerScore
@@ -37,5 +43,12 @@ const player = function (name, idNum) {
     };
 
     return {getName, roll};
-}
+};
+
+// Example players
+const playerOne = player('Ryan', 1);
+const playerTwo = player('Liz', 2);
+
+const btnRoll = document.querySelector('.btn--roll');
+btnRoll.addEventListener('click', (activePlayer === 1 ? playerOne.roll : playerTwo.roll))
 
