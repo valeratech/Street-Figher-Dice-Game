064fdec64abce48f9c440d3df470adb5a773fb4e
Ryan Valera <valeraryan@gmail.com>
2022-11-02 14:30:50 -0700

    Added positioning of character-select boxes and styling

--- menu.js
+++ menu.js
@@ -1,3 +1,15 @@
 const playerSelect = (function () {
-    // add code
+    const contPlayerOne = document.getElementById('container--p1');
+    const contPlayerTwo = document.getElementById('container--p2');
+    const contCharacterOne = document.getElementById('character--p1--p2');
+    const contCharacterTwo = document.getElementById('character--p1--p2');
+
+    const updateDisplay = function() {
+
+    }
+    return {updateDisplay}
 })();
+
+const selectContainer = document.getElementById('player-select')
+selectContainer.addEventListener('click', (e) => console.log(e.target))
+
