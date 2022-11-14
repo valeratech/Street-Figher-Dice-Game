c00b47d2b04bc1ff5711d6aaf8120796bd2f51f0
Ryan Valera <valeraryan@gmail.com>
2022-11-14 00:06:10 -0800

    Added image-profiles. Reized image-profiles to fit inside container

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-const playerSelect = (function () {
+const characterScreen = (function () {
     const contPlayerOne = document.getElementById('container--p1');
     const contPlayerTwo = document.getElementById('container--p2');
     const contCharacterOne = document.getElementById('character--p1--p2');
@@ -9,40 +9,51 @@ const playerSelect = (function () {
         if (e.target.id === 'char--ryu') {
             contPlayerOne.removeChild(contPlayerOne.firstChild);
             let img = document.createElement('img');
-            img.src = 'ryu_transparent_thumb.png';
+            img.src = 'ryu_transparent_profile.png';
             contPlayerOne.appendChild(img);
         } else if (e.target.id === 'char--chun') {
             contPlayerOne.removeChild(contPlayerOne.firstChild);
             let img = document.createElement('img');
-            img.src = 'chun_transparent_thumb.png';
+            img.src = 'chun_transparent_profile.png';
             contPlayerOne.appendChild(img);
         } else if (e.target.id === 'char--ken') {
             contPlayerOne.removeChild(contPlayerOne.firstChild);
             let img = document.createElement('img');
-            img.src = 'ken_transparent_thumb.png';
+            img.src = 'ken_transparent_profile.png';
             contPlayerOne.appendChild(img);
         } else if (e.target.id === 'char--akuma') {
             contPlayerOne.removeChild(contPlayerOne.firstChild);
             let img = document.createElement('img');
-            img.src = 'akuma_transparent_thumb.png';
+            img.src = 'akuma_transparent_profile.png';
             contPlayerOne.appendChild(img);
         } else if (e.target.id === 'char--sakura') {
             contPlayerOne.removeChild(contPlayerOne.firstChild);
             let img = document.createElement('img');
-            img.src = 'sakura_transparent_thumb.png';
+            img.src = 'sakura_transparent_profile.png';
             contPlayerOne.appendChild(img);
         } else if (e.target.id === 'char--dan') {
             contPlayerOne.removeChild(contPlayerOne.firstChild);
             let img = document.createElement('img');
-            img.src = 'dan_transparent_thumb.png';
+            img.src = 'dan_transparent_profile.png';
             contPlayerOne.appendChild(img);
         }
     }
 
-    return {updateDisplay}
+    const selectPlayer = function(e) {
+
+    }
+
+    return {updateDisplay, selectPlayer}
 })();
 
 
 const selectContainer = document.getElementById('player-select');
-selectContainer.addEventListener('mouseover', playerSelect.updateDisplay);
+selectContainer.addEventListener('mouseover', characterScreen.updateDisplay);
 
+selectContainer.addEventListener('click', (e) => {
+    if (e.target.id === 'char--ryu') {
+        console.log('hello');
+        const myButton = document.querySelector('#char--ryu');
+        myButton.disabled = true;
+    }
+})
